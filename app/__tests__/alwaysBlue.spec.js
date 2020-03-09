29b04a130e397d0065a2bee3dcd5a40288d5cc1c
Andy Powell <andypowell@andys-mbp.lan>
2020-03-09 17:47:45 -0500

    toss erorrs

--- app/__tests__/alwaysBlue.spec.js
+++ app/__tests__/alwaysBlue.spec.js
@@ -1,10 +1,24 @@
 describe("Example test", () => {
-    test("it should always return blue", () => {
-      const input = "blue"
-  
-      const output = "blue-ish";
-  
-      expect(input === output);
-  
-    });
+  function blueOrNot(shouldThrow = false) {
+      if (shouldThrow) {
+        console.log(shouldThrow);
+        throw new Error('shouldThrow was true');
+      }
+      return 'blue';
+  }
+
+  it('should throw if passed true', () => {
+    try {
+      blueOrNot(true);
+    } catch (error) {
+      expect(error).toEqual(new Error('shouldThrow was true'));
+    }
+  });
+  test("it should always return blue", () => {
+    const input = "bluer"
+
+    const output = blueOrNot(false);
+
+    expect(input).toEqual(output);
+  });
 });
