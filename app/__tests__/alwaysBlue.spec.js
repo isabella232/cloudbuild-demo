22bcfb2dac236b5b607ab8aebead6a785fbaee74
Andy Powell <andypowell@andys-mbp.lan>
2020-03-09 13:09:55 -0500

    cloud build

---
+++ app/__tests__/alwaysBlue.spec.js
@@ -0,0 +1,10 @@
+describe("Example test", () => {
+    test("it should always return blue", () => {
+      const input = "blue"
+  
+      const output = "blue";
+  
+      expect(input === output);
+  
+    });
+});
